d4b39eb671562401bd422e2d82668bc9e15fa5eb
Cameron Stitt <cameron@cam.st>
2018-02-16 16:30:20 +1000

    Start building walker to retrieve vars and templates

--- template/template.go
+++ template/template.go
@@ -1,6 +1,10 @@
 package template
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/hashicorp/hil"
 	"github.com/hashicorp/hil/ast"
 	"github.com/openpixel/rise/config"
@@ -30,12 +34,70 @@ func (t *Template) buildConfig() *hil.EvalConfig {
 	}
 }
 
+func NewVariable(name string) (interface{}, error) {
+	if strings.HasPrefix(name, "var.") {
+		return "Var", nil
+	} else if strings.HasPrefix(name, "tmpl") {
+		return "Template", nil
+	}
+
+	return nil, errors.New("Unknown variable access")
+}
+
 // Render will parse the provided text and interpolate the known variables/functions
 func (t *Template) Render(text string) (hil.EvaluationResult, error) {
 	tree, err := hil.Parse(text)
 	if err != nil {
 		return hil.InvalidResult, err
 	}
+
+	hil.Walk(text, func(data *hil.WalkData) error {
+		var resultErr error
+
+		fn := func(n ast.Node) ast.Node {
+			if resultErr != nil {
+				return n
+			}
+
+			switch vn := n.(type) {
+			case *ast.VariableAccess:
+				v, err := NewVariable(vn.Name)
+				if err != nil {
+					resultErr = err
+					return n
+				}
+				fmt.Println(v)
+			case *ast.Index:
+				if va, ok := vn.Target.(*ast.VariableAccess); ok {
+					v, err := NewVariable(va.Name)
+					if err != nil {
+						resultErr = err
+						return n
+					}
+					fmt.Println(v)
+				}
+				if va, ok := vn.Key.(*ast.VariableAccess); ok {
+					v, err := NewVariable(va.Name)
+					if err != nil {
+						resultErr = err
+						return n
+					}
+					fmt.Println(v)
+				}
+			default:
+				return n
+			}
+			return n
+		}
+
+		data.Root.Accept(fn)
+
+		if resultErr != nil {
+			return resultErr
+		}
+
+		return nil
+	})
 
 	result, err := hil.Eval(tree, t.buildConfig())
 	if err != nil {
