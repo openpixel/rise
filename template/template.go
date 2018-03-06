d3fdf9f55ae2ec7d66bacf69f3c85580a9f0b709
Cameron Stitt <cameron@cam.st>
2018-03-06 16:34:35 +1000

    Cleanup template processing

--- template/template.go
+++ template/template.go
@@ -1,7 +1,6 @@
 package template
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/hashicorp/hil"
@@ -42,59 +41,11 @@ func (t *Template) buildConfig() *hil.EvalConfig {
 	}
 }
 
-func (t *Template) visitFn(resultErr error) ast.Visitor {
-
-	return func(n ast.Node) ast.Node {
-		if resultErr != nil {
-			return n
-		}
-
-		var varName string
-
-		switch vn := n.(type) {
-		case *ast.VariableAccess:
-			varName = vn.Name
-		case *ast.Index:
-			if va, ok := vn.Target.(*ast.VariableAccess); ok {
-				varName = va.Name
-			}
-			if va, ok := vn.Key.(*ast.VariableAccess); ok {
-				varName = va.Name
-			}
-		default:
-			return n
-		}
-
-		if strings.HasPrefix(varName, "tmpl.") {
-			if t.templates[varName].Type == ast.TypeString {
-				value := t.templates[varName].Value.(string)
-
-				nodeTree, err := hil.Parse(value)
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				newValue, err := hil.Eval(nodeTree, t.buildConfig())
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				literalN, err := ast.NewLiteralNode(newValue.Value.(string), n.Pos())
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				return literalN
-			}
-		}
-
-		return n
-	}
-}
-
 // Render will parse the provided text and interpolate the known variables/functions
 func (t *Template) Render(text string) (hil.EvaluationResult, error) {
 	var resultErr error
+
+	config := t.buildConfig()
 
 	tree, err := hil.Parse(text)
 	if err != nil {
@@ -123,25 +74,12 @@ func (t *Template) Render(text string) (hil.EvaluationResult, error) {
 		}
 
 		if strings.HasPrefix(varName, "tmpl.") {
-			if t.templates[varName].Type == ast.TypeString {
-				value := t.templates[varName].Value.(string)
-				nodeTree, err := hil.Parse(value)
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				newValue, err := hil.Eval(nodeTree, t.buildConfig())
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				newN, err := hil.ParseWithPosition(newValue.Value.(string), n.Pos())
-				if err != nil {
-					resultErr = err
-					return n
-				}
-				return newN
+			resultN, err := processTemplateNode(n, t.templates[varName], config)
+			if err != nil {
+				resultErr = err
+				return n
 			}
+			return resultN
 		}
 
 		return n
@@ -150,14 +88,37 @@ func (t *Template) Render(text string) (hil.EvaluationResult, error) {
 	tree.Accept(fn)
 
 	if resultErr != nil {
-		fmt.Println(resultErr)
 		return hil.InvalidResult, resultErr
 	}
 
-	result, err := hil.Eval(tree, t.buildConfig())
+	result, err := hil.Eval(tree, config)
 	if err != nil {
 		return hil.InvalidResult, err
 	}
 
 	return result, nil
+}
+
+func processTemplateNode(original ast.Node, template ast.Variable, config *hil.EvalConfig) (new ast.Node, err error) {
+	switch template.Type {
+	case ast.TypeString:
+		var root ast.Node
+		var newValue hil.EvaluationResult
+		value := template.Value.(string)
+		root, err = hil.Parse(value)
+		if err != nil {
+			break
+		}
+		newValue, err = hil.Eval(root, config)
+		if err != nil {
+			break
+		}
+		new, err = hil.ParseWithPosition(newValue.Value.(string), original.Pos())
+		if err != nil {
+			break
+		}
+		return new, nil
+	}
+
+	return original, err
 }
